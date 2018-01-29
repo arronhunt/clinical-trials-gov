83ee18c791b5e25594702de251e1ecf943d9d1c8
Arron Hunt <arronjhunt@gmail.com>
2018-01-28 16:35:50 -0800

    Fixed example

--- example.js
+++ example.js
@@ -1,9 +1,7 @@
 const ClinicalTrial = require('./src/');
 
-// ClinicalTrial.search({condition: "Asthma"}).then(studies => {
-//     studies.forEach((study, index) => {
-//         console.log(index, study.title);
-//     });
-// });
-
-ClinicalTrial.searchConditions('Asth').then(console.log)
+ClinicalTrial.search({condition: "Asthma"}).then(studies => {
+    studies.forEach((study, index) => {
+        console.log(index, study.title);
+    });
+});
