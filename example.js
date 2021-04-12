ea8658e31f5f52387b5c840cbab81ae86b31e84d
Arron Hunt <arronjhunt@gmail.com>
2021-04-12 11:03:36 -0700

    Ran prettier, updated dependencies

--- example.js
+++ example.js
@@ -1,7 +1,7 @@
-const ClinicalTrial = require('./src/');
+const ClinicalTrial = require("./src/");
 
-ClinicalTrial.search({condition: "Asthma"}).then(studies => {
-    studies.forEach((study, index) => {
-        console.log(index, study.title);
-    });
+ClinicalTrial.search({ condition: "Asthma" }).then((studies) => {
+  studies.forEach((study, index) => {
+    console.log(index, study.title);
+  });
 });
